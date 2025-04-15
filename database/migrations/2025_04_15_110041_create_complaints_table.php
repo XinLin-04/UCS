65d312cc900220aa144f08f5ffc1c966e431ef57
XinLin-04 <2203314@1utar.my>
2025-04-15 23:59:39 +0800

    Setup

---
+++ database/migrations/2025_04_15_110041_create_complaints_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateComplaintsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('complaints', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('content');
+            $table->timestamp('created_at')->useCurrent();
+            $table->foreignId('user_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('complaints');
+    }
+}
