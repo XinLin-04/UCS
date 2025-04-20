ff5d54fab04c5009abe85711182e02d718740b93
ws1023 <leeweisen80@gmail.com>
2025-04-20 21:45:03 +0800

    delete admin and notify table

--- database/migrations/2025_04_15_110017_create_users_table.php
+++ database/migrations/2025_04_15_110017_create_users_table.php
@@ -14,15 +14,14 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id(); // Auto-incrementing ID
-            $table->string('name'); // Name of the user
-            $table->string('email')->unique(); // Unique email address
+            $table->id(); 
+            $table->string('name');
+            $table->string('email')->unique(); // Unique email addres
             $table->string('password'); // Password field
             $table->enum('role', ['user', 'admin'])->default('user'); // User role (user or admin)
             $table->rememberToken(); //for remember me functionality, cookie based authentication
             $table->timestamps(); //created_at and updated_at timestamps
             $table->timestamp('email_verified_at')->nullable(); // Email verification timestamp
-            $table->string('profile_picture')->nullable(); // Profile picture URL
         });
     }
 
