825ad4d171ac7c4c4c54364406afbb3c67acc6b6
XinLin-04 <2203314@1utar.my>
2025-04-20 18:15:52 +0800

    Change the email input type

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -50,7 +50,7 @@
                                     class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
 
                                 <div class="col-md-6">
-                                    <input id="email" type="email"
+                                    <input id="email" type="text"
                                         class="form-control @error('email') is-invalid @enderror" name="email"
                                         value="{{ old('email') }}"  autocomplete="email" autofocus>
 
