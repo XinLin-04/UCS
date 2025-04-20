0e0c31ba35354c3b0236b552e9bb9a0f4c289e82
HaoJun0528 <bhj020528@1utar.my>
2025-04-20 21:22:14 +0800

    modified login blade and searchResultblade

--- resources/views/search/searchResult.blade.php
+++ resources/views/search/searchResult.blade.php
@@ -1,27 +1,21 @@
-<!-- resources/views/search/results.blade.php -->
-
 @extends('layouts.header')
 
 @section('content')
-    <h1>Search Results</h1>
+    <div class="container my-5">
+        <h1>Search Results for "{{ $query }}"</h1>
 
-    <!-- Display Posts -->
-    @if($posts->count())
-        <h2>Posts</h2>
-        <ul>
-            @foreach($posts as $post)
-                <li>{{ $post->title }}</li>
-            @endforeach
-        </ul>
-    @endif
-
-    <!-- Display Products -->
-    @if($products->count())
-        <h2>Products</h2>
-        <ul>
-            @foreach($products as $product)
-                <li>{{ $product->name }}</li>
-            @endforeach
-        </ul>
-    @endif
+        @if ($posts->isEmpty())
+            <p>No posts found matching your search query.</p>
+        @else
+            <div class="list-group">
+                @foreach ($posts as $post)
+                    <a href="{{ route('posts.show', $post->id) }}" class="list-group-item list-group-item-action">
+                        <h5 class="mb-1">{{ $post->title }}</h5>
+                        <p class="mb-1">{{ Str::limit($post->content, 100) }}</p>
+                        <small>Posted on {{ $post->created_at->format('M d, Y') }}</small>
+                    </a>
+                @endforeach
+            </div>
+        @endif
+    </div>
 @endsection
