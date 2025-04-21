32ebccfe2ac220d0e625d8510f888adf7affcf52
AndySee04 <andystkei@gmail.com>
2025-04-21 10:22:55 +0800

    import RegisterController in web.php

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\SearchController;
 use App\Http\Controllers\ComplaintController;
 use App\Http\Controllers\CommentController;
+use App\Http\Controllers\Auth\VerificationController;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\UserController;
 
